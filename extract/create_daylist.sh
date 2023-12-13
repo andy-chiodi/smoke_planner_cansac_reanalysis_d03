d589c70a0407ed0b04beda41522d88c5d86c9274
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2023-12-13 14:29:33 -0800

    tweak

--- extract/create_daylist.sh
+++ extract/create_daylist.sh
@@ -4,6 +4,6 @@
 
 # Create daylist files for a given year
 for run in $(ls -d /fire5/cansac_reanalysis/$1/*); do
-  ls -1 ${run} | cut -c12-21 | sort -u | grep "^19" > daylist_$1_${run##*/}.txt 
+  ls -1 ${run} | cut -c12-21 | sort -u | grep "^19" > daylist_$1_${run##*/}
 done
 
