6aa01968cc27825988fa25440d124c2be6db8ead
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2024-02-28 10:04:26 -0800

    improved wrf_extract.py

--- extract/wrf_extract.py
+++ extract/wrf_extract.py
@@ -1,5 +1,5 @@
 # file wrf_extract.py
-# command line usage:  python wrf_extract.py daylist_pattern main_CANSAC.jnl
+# command line usage:  python wrf_extract.py year main_CANSAC.jnl
 # where "daylist" and "hourlist" are file names of files containing lists of days/hours to repeat over
 # depends on ferret .jnl files: recipe_CANSAC.jnl and vi_CANSAC.jnl, as well as the variable-specific
 # ferret script that saves to netcdf, e.g. main_CANSAC.jnl (for mixing, trans. wind, vent. index and 10m wspd)
@@ -15,17 +15,15 @@
 # alpha.nc    (saved from a sample wrfout file; needed to adjust wind direction to Cartesian coordinates)
 # wrf_zaxes.nc (created by define_axes.jnl)
 
-# [START_HERE] source WRF repository directory location info (possibly no write permission)
-###source_dir  = '/storage/chiodi/CANSAC_reanalysis/d03/source_data/'
-source_dir_base = '/fire5/cansac_reanalysis/1981/'
-# working directory info (w/ write permission)
-###working_dir = '/storage/chiodi/CANSAC_reanalysis/d03/extract/'
-working_dir = '/fire8/jonc/Projects/smoke_planner_cansac_reanalysis_d03/extract/'
-# where the output netcdf files will be saved
-###save_dir = '/storage/chiodi/CANSAC_reanalysis/d03/output_data/extract/'
-save_dir = '/fire8/jonc/CANSAC_reanalysis/d03/output_data/extract/'
-# [end of directory changes]
-
+# NOTE:  CANSAC files are in different directories
+# [jonc@fire extract]$ ls /fire2/cansac_reanalysis/
+# 2001  2002  2003  2004  2005  2006  2007  2008  2009  2010  2011
+# [jonc@fire extract]$ ls /fire3/cansac_reanalysis/
+# 2012  2013  2014  2015  2016  2017  2018  2019  2020  2021
+# [jonc@fire extract]$ ls /fire4/cansac_reanalysis/
+# 1988  1989  1990  1991  1992  1993  1994  1995  1996  1997  1998  1999  2000
+# [jonc@fire extract]$ ls /fire5/cansac_reanalysis/
+# 1980  1981  1982  1983  1984  1985  1986  1987
 
 # file naming info
 pre = 'wrfout_d03_'
@@ -56,29 +54,52 @@ if __name__ == '__main__':
     logging.basicConfig(
       filename=jons_log, 
       format='%(asctime)s %(levelname)-8s %(message)s',
-      level=logging.DEBUG,
+      level=logging.INFO,
       datefmt='%Y-%m-%d %H:%M:%S'
     )
 
 
-    logging.debug("sys.argv = " + " ".join(sys.argv))
+    logging.info("sys.argv = " + " ".join(sys.argv))
+
+    year = int(sys.argv[1])
+
+    if ( year in [1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987] ):
+        source_dir_base = '/fire5/cansac_reanalysis/' + str(year) + '/'
+    elif ( year in [1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000] ):
+        source_dir_base = '/fire4/cansac_reanalysis/' + str(year) + '/'
+    elif ( year in [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021] ):
+        source_dir_base = '/fire3/cansac_reanalysis/' + str(year) + '/'
+    elif ( year in [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011] ):
+        source_dir_base = '/fire2/cansac_reanalysis/' + str(year) + '/'
+    else:
+        logging.error("Year " + str(year) + " is not recognized")
+        sys.exit("Year " + str(year) + " is not recognized")
+
+    # [START_HERE] source WRF repository directory location info (possibly no write permission)
+    ###source_dir_base = '/fire5/cansac_reanalysis/1981/'
+    # working directory info (w/ write permission)
+    working_dir = '/fire8/jonc/Projects/smoke_planner_cansac_reanalysis_d03/extract/'
+    # where the output netcdf files will be saved
+    save_dir = '/fire8/jonc/CANSAC_reanalysis/d03/output_data/extract/'
+    # [end of directory changes]
 
     # Create a list of hours (see jons_python_snippets.md)
     hours = [str(hour).zfill(2) for hour in range(24)]
 
     # Create an array of daylist files
-    pattern = str(sys.argv[1]) + '*'
+    ###pattern = str(sys.argv[1]) + '*'
+    pattern = 'daylist_' + str(year) + '*'
     directory = os.getcwd()
     daylist_files = glob.glob(directory + "/" + pattern)
 
     for daylist_file in daylist_files:
 
-        logging.debug("daylist_file = " + daylist_file)
+        logging.info("daylist_file = " + daylist_file)
         dname = os.path.basename(daylist_file)
-        logging.debug("dname = " + dname)
+        logging.info("dname = " + dname)
 
         # Use a list comprehension to extract parts of this name
-        name, year, run_dir = [s for s in dname.split("_")]
+        name, yearString, run_dir = [s for s in dname.split("_")]
 
         # read file containing list of days
         with open(dname) as f:
@@ -86,11 +107,11 @@ if __name__ == '__main__':
 
         # read run subdirectory
         source_dir = source_dir_base + run_dir + "/"
-        logging.debug("source_dir = " + source_dir)
+        logging.info("source_dir = " + source_dir)
 
         # save str name of .jnl ferret script that this shell will run
         fname = str(sys.argv[2])
-        logging.debug("fname = " + fname)
+        logging.info("fname = " + fname)
 
         days  = [x.strip() for x in days]
         hours = [x.strip() for x in hours]
@@ -107,9 +128,10 @@ if __name__ == '__main__':
 
         for x in days:
             day  = x.strip()
+            logging.info("Working on %s", day)
             for y in hours:
                 hour = y.strip()
-                logging.debug("Working on %s %s:00", day, hour)
+                logging.debug("Working on %s %d:00", day, hour)
     #            fn = pre+day+'-'+hour+post
     #            gzd = source+day+'/'+fn
     #            unc = working+fn.strip('.gz')
@@ -134,10 +156,12 @@ if __name__ == '__main__':
                       os.remove(f_out)
                       #print('removing source file')
                    except OSError:
+                      logging.error("Did not remove " + f_out)
                       lfid.write('Did not remove '+f_out+'\n')
                       pass
     
                 except:
+                   logging.error("Could not find " + f_in )
                    lfid.write('Could not find '+f_in+'\n')
  
 # note: gridmet has its own calc. path, merge comes after reshape & todaily
@@ -146,5 +170,5 @@ if __name__ == '__main__':
     lfid.write(str(et)+'\n')
     lfid.close()
 
-# command line usage:  python wrf_extract.py daylist_pattern main_CANSAC.jnl
+# command line usage:  python wrf_extract.py year main_CANSAC.jnl
 
